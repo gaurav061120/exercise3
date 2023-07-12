a03b1d76fb5645dc439843f6b7a238c55efdd449
gaurav061120 <gaurav.mishra0611@gmail.com>
2023-07-12 11:46:40 +0530

    first commit

---
+++ ConsoleAppexercise3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppexercise3
+{
+    internal class Program
+    {
+        static void Main()
+
+        {
+            double length = 5.0;
+            double breadth = 3.0;
+            double area = 15.0;
+            Rectangle rectangle = new Rectangle(length, breadth);
+            Console.WriteLine("Area :" + rectangle.CalculateArea());
+            Console.WriteLine("Length :" + rectangle.CalculateLength(area , breadth));
+            Console.WriteLine("Bradth :" + rectangle.CalculateBreadth(area, length));
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
+       
---
+++ ConsoleAppexercise3/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppexercise3
+{
+    public class Rectangle
+    {
+        double length;
+        double breadth;
+        public Rectangle(double length, double breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+        public double CalculateArea()
+        {
+            return length * breadth;
+
+        }
+        public double CalculateLength(double area, double breadth)
+        {
+            return area / breadth;
+
+        }
+        public double CalculateBreadth(double length, double area)
+        {
+            return area / length;
+           
+        }
+    }
+}
+
+
+   
